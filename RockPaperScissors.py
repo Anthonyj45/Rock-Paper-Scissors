94449f2427bbb7d90268ec17a24702eabd21864c
Anthonyj45 <moronta7@gmail.com>
2023-07-02 20:58:44 -0400

    I noticed that I could make the code more organized.

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,36 +1,30 @@
 
 import random
 
-def piedras_Papel(user_selected):
-    options = ["Rock", "Paper", "Scissors"]
-    """
-    In  a variable named as random_solution, it will be saved the function random.choice which return a random element from our list.
-    """
-    random_Solution = random.choice(options)
-    """
-    It's necessary to save in lowercase two new variables the strings that were taken from the user.
-    If the user submit a string with a different case, then will not be any trouble.
-    And the code becomes simpler because of the .lower() attribute is called once, and not multiple times.
-    
-    The .capitalize() attribute will modify the string, and it will always start with an Uppercase.
-    """
-    user_Sel = user_selected.lower().capitalize()
-    ran_Sol = random_Solution.lower().capitalize()
-
-    if user_Sel == ran_Sol:
-        print(f"You chose {user_Sel}, and the computer chose {ran_Sol}, So it is a draw.")
-    elif (user_Sel == "Rock" and ran_Sol == "Scissors") or (user_Sel == "Paper" and ran_Sol == "Rock") or (user_Sel == "Scissors" and ran_Sol == "Paper"):
-        print(f"You chose {user_Sel}, and the computer chose {ran_Sol}, So you won.")
-    elif (user_Sel == "Rock" and ran_Sol == "Paper") or (user_Sel == "Paper" and ran_Sol == "Scissors") or (user_Sel == "Scissors" and ran_Sol == "Rock"):
-        print(f"You chose {user_Sel}, and the computer chose {ran_Sol}, So you lost.")
-    else:
-        print("You didn't select a correct option. Please, Try again..")
-
-if __name__ == '__main__':
+def user_selected(user_info, options):
     while True:
-        user_selected = input("Please, choose between Rock, Paper or Scissors: ")
-        if user_selected == "Rock" or user_selected == "Paper" or user_selected =="Scissors":
-            piedras_Papel(user_selected)
+        if user_info in options:
             break
         else:
+            print("You didn't select a valid option. Please, try again.")
+            user_info = input("Please, choose between Rock, Paper and Scissors: ")
             continue
+
+def play_game(user_info, options):
+    random_option = random.choice(options)
+    if user_info == random_option:
+        print(f"Wow! You chose {user_info}, and the computer chose {random_option}. Then, I think we have a draw here.")
+    elif user_info == "rock" and random_option == "scissors" or user_info == "paper" and random_option == "rock" or user_info == "scissors" and random_option == "paper":
+        print(f"Amazing! You chose {user_info}, and the computer chose {random_option}. For that reason, you are the winner.")
+    else:
+        print(f"Sorry, you chose {user_info}, and the computer chose {random_option}. You loose this time.")
+    
+def main():
+    options = ["rock", "paper", "scissors"]
+    user_info = input("Please, choose between Rock, Paper and Scissors: ")
+    user_info = user_info.lower()
+    user_selected(user_info, options)
+    play_game(user_info, options)
+
+if __name__ == '__main__':
+    main()
